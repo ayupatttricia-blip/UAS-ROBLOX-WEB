f119c18a9100fa942d25304940b5c5b4c6519e29
gitanaura <gitanaura12@gmail.com>
2026-05-26 13:57:23 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Splash from "./pages/Splash";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
 }
 
 export default App;
